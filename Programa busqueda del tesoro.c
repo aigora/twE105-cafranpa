e91d07d9a380198620d0583c47b6eacf686c46b2
fdelgadol <47789266+fdelgadol@users.noreply.github.com>
2019-04-08 23:02:19 +0200

    Create Programa busqueda del tesoro.c

---
+++ Programa busqueda del tesoro.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+	/* Este programa estará basado en una busqueda del tesoro en base a funciones con punteros 
+	y a estructuras que alberguen la posicion del tesoro y del usuario.
+	Los punteros ayudaran a modificar las estructuras con las funciones gracias al 
+	paso por referencia*/
+	return 0;
+}
+//lo dejo apuntado para que no se me olvide la idea
