c42f0953a99e5ba944053f3466b6803ff41946f9
fdelgadol <47789266+fdelgadol@users.noreply.github.com>
2019-04-09 17:41:49 +0200

    Update Programa busqueda del tesoro.c
    Pruebas varias

--- Programa busqueda del tesoro.c
+++ Programa busqueda del tesoro.c
@@ -10,6 +10,7 @@ void enterrar(posicion *tesoro);
 void busqueda(posicion *tesoro, posicion *usuario);
 int main()
 {
+	int cnt;
 	posicion tesoro, usuario;
 	enterrar(&tesoro);
 	printf("Bienvenido a 'la busqueda del tesoro'\n");
@@ -21,7 +22,13 @@ int main()
 	printf("Tu objetivo, como habras podido deducir, es encontrar el tesoro\n");
 	system("pause");
 	printf("Para ello te voy a dar 1 minuto\n");
-	printf("Buena suerte\n");
+	printf("Buena suerte(pulse cuando esté preparado)\n");
+	system("pause");
+	for(cnt = 3; cnt >= 1; cnt--){
+	system("cls");
+	printf("\n %d \n", cnt);
+	sleep(1);
+	} 
 	busqueda(&tesoro, &usuario);
 	return 0;
 }
@@ -34,5 +41,23 @@ void enterrar(posicion *tesoro)
 }
 void busqueda(posicion *tesoro, posicion *usuario)
 {
-	
+	clock_t t=0,ts=0;
+	int s=0, i=0;
+	ts=clock()+CLOCKS_PER_SEC;
+//	printf("%i\n",t);
+	do
+	{
+
+			if((t=clock())>=ts)
+			{
+				s++;
+			//	system("cls");
+				ts=t+CLOCKS_PER_SEC;
+				printf("%i\n",t);
+			//	scanf("%i",&i);
+			//	printf("%i\n",i);
+			}
+		}	
+
+	while(t/1000<=10);
 }
