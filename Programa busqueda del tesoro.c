bbe19551f0f156269c6021d427b50d393a432387
fdelgadol <47789266+fdelgadol@users.noreply.github.com>
2019-04-09 16:01:02 +0200

    Update Programa busqueda del tesoro.c

--- Programa busqueda del tesoro.c
+++ Programa busqueda del tesoro.c
@@ -1,12 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+typedef struct
+{
+	int x;
+	int y;
+}posicion;
+void enterrar(posicion *tesoro);
 int main()
 {
+	posicion tesoro, usuario;
+	enterrar(&tesoro);
+	printf("Bienvenido a 'la busqueda del tesoro'\n");
+	system("pause");
+	printf("Se ha enterrado en un mapa de 100*100 un tesoro\n");
+	system("pause");
+	printf("Comienzas en el origen de coordenadas\n");
+	system("pause");
+	printf("Tu objetivo, como habras podido deducir, es encontrar el tesoro\n");
+	
 	/* Este programa estará basado en una busqueda del tesoro en base a funciones con punteros 
 	y a estructuras que alberguen la posicion del tesoro y del usuario.
 	Los punteros ayudaran a modificar las estructuras con las funciones gracias al 
 	paso por referencia*/
 	return 0;
 }
-//lo dejo apuntado para que no se me olvide la idea
+void enterrar(posicion *tesoro)
+{
+	posicion *res=tesoro;
+	srand(time(NULL));
+	res->x=rand() % 100+0;
+	res->y=rand() % 100+0;
+}
