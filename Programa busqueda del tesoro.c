24eaeaa187fd06332254ce9a160e55059524e9a9
fdelgadol <47789266+fdelgadol@users.noreply.github.com>
2019-04-09 20:46:28 +0200

    Update Programa busqueda del tesoro.c

--- Programa busqueda del tesoro.c
+++ Programa busqueda del tesoro.c
@@ -14,17 +14,28 @@ int main()
 	posicion tesoro, usuario;
 	enterrar(&tesoro);
 	printf("Bienvenido a 'la busqueda del tesoro'\n");
-	system("pause");
+	sleep(3);
+	system("cls");
 	printf("Se ha enterrado en un mapa de 100*100 un tesoro\n");
-	system("pause");
+	sleep(3);
+	system("cls");
 	printf("Comienzas en el origen de coordenadas\n");
-	system("pause");
+	sleep(3);
+	system("cls");
 	printf("Tu objetivo, como habras podido deducir, es encontrar el tesoro\n");
+	sleep(3);
+	system("cls");
+	printf("Para ello te voy a dar 20 rondas\n");
+	printf("En cada una de ellas podras mover tu personaje cuantas unidades quieras\n");
+	printf("Para ello tienes que pulsar una de las teclas direccionales, y el numero de unidades que quieras avanzar\n");
+	printf("(Pulse enter cuando hayas entendido las instrucciones)\n");
 	system("pause");
-	printf("Para ello te voy a dar 1 minuto\n");
+	system("cls");
 	printf("Buena suerte(pulse cuando esté preparado)\n");
 	system("pause");
-	for(cnt = 3; cnt >= 1; cnt--){
+	system("cls");
+	for(cnt = 3; cnt >= 1; cnt--)
+	{
 	system("cls");
 	printf("\n %d \n", cnt);
 	sleep(1);
@@ -41,23 +52,35 @@ void enterrar(posicion *tesoro)
 }
 void busqueda(posicion *tesoro, posicion *usuario)
 {
-	clock_t t=0,ts=0;
-	int s=0, i=0;
-	ts=clock()+CLOCKS_PER_SEC;
-//	printf("%i\n",t);
+	posicion *t=tesoro,*u=usuario;
+	u->x=u->y=0;
+	int i,n;
+	char l;
 	do
 	{
-
-			if((t=clock())>=ts)
+		i++;
+		printf("Ronda %i\n",i);
+		sleep(2);
+		system("cls");
+		proximidad(tesoro,usuario);//hacer funcion void proximidad(posicion *tesoro, posicion *usario)
+		printf("usuario:\n\tx:")
+		scanf("%c%i",&l&n);
+		switch(l)
+		{
+			case 'w':
 			{
-				s++;
-			//	system("cls");
-				ts=t+CLOCKS_PER_SEC;
-				printf("%i\n",t);
-			//	scanf("%i",&i);
-			//	printf("%i\n",i);
+				avancevertical(n,usuario);//hacer funcion void avancevertical(int n, posicion *usuario)
+				break;		
 			}
-		}	
-
-	while(t/1000<=10);
+			case 'd':
+			{
+				avancehorizontal(n,usuario);//hacer funcion void avancehorizontal(int n, posicion *usuario)
+			}
+			case 's':
+			{
+				avancevertical(-n,usuario);//hacer funcion void avancevertical(int n, posicion *usuario)
+				break;		
+			}	
+		}
+	}
 }
